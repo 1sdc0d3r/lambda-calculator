047ccb04ee84152678ec19abb58df323a6e0cd3b
Braden <fish2355@hotmail.com>
2019-12-06 19:29:58 -0700

    first commit, not started

--- src/components/ButtonComponents/NumberButtons/NumberButton.js
+++ src/components/ButtonComponents/NumberButtons/NumberButton.js
@@ -1,9 +1,14 @@
 import React from "react";
-
-const NumberButton = () => {
+import data from "../../../data";
+console.log(data);
+const NumberButton = props => {
+  const { data } = props;
   return (
     <>
       {/* Display a button element rendering the data being passed down from the parent container on props */}
+      <button>{data.numbers[0]}</button>
     </>
   );
 };
+
+export default NumberButton;
--- src/components/ButtonComponents/NumberButtons/Numbers.js
+++ src/components/ButtonComponents/NumberButtons/Numbers.js
@@ -1,7 +1,8 @@
 import React from "react";
 
+
 //import any components needed
-// example of import from data.js. Note all the ../   This is how we move through folders. 
+// example of import from data.js. Note all the ../   This is how we move through folders.
 /* 
 import { numbers } from '../../../data' 
 */
@@ -17,3 +18,5 @@ const Numbers = () => {
     </div>
   );
 };
+
+export default Numbers;
